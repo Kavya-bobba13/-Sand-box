730644af9d000a5a72713389cf1398e344939c67
kavya bobba <kavyabobba13@gmail.com>
2023-03-09 15:01:28 +0530

    size-bhk configured

--- frontend/js/rent_flat.js
+++ frontend/js/rent_flat.js
@@ -9,11 +9,19 @@ drop_down_bud.forEach(element => {
     });
     
     const drp_btn=document.getElementsByClassName("dropdown-toggle")[0];
-    if(drp_btn.innerText!=e.target.innerText){
-    
-    drp_btn.innerText=e.target.innerText;
-    console.log(e.target);
-    if(e.target.innerText=="1k - 10k"){
+    const drp_btn2=document.getElementsByClassName("dropdown-toggle")[1];
+    console.log(drp_btn.innerText);
+    console.log(drp_btn2.innerText);
+    if(drp_btn.innerText!=e.target.innerText && drp_btn2.innerText!=e.target.innerText ){
+        if(e.target.innerText.indexOf("bhk")>-1){
+            drp_btn2.innerText=e.target.innerText;
+        }
+        else if(e.target.innerText.indexOf("bhk")==-1){
+            drp_btn.innerText=e.target.innerText;
+        }
+        
+        console.log(e.target);
+        if(e.target.innerText=="1k - 10k"){
         
         items.forEach(val => { 
             let cost=val.getElementsByClassName("bottom")[0].getElementsByTagName("div")[0].innerText;
@@ -23,8 +31,8 @@ drop_down_bud.forEach(element => {
                 val.style.display="none";
             }
         });        
-    }
-    else if(e.target.innerText=="10k - 50k"){
+        }
+        else if(e.target.innerText=="10k - 50k"){
         items.forEach(val => { 
             let cost=val.getElementsByClassName("bottom")[0].getElementsByTagName("div")[0].innerText;
             console.log(cost.substring(1));
@@ -33,8 +41,8 @@ drop_down_bud.forEach(element => {
                 val.style.display="none";
             }
         });        
-    }
-    else if(e.target.innerText=="50k - 1Lac"){
+        }
+        else if(e.target.innerText=="50k - 1Lac"){
         items.forEach(val => { 
             let cost=val.getElementsByClassName("bottom")[0].getElementsByTagName("div")[0].innerText;
             console.log(cost.substring(1));
@@ -43,8 +51,48 @@ drop_down_bud.forEach(element => {
                 val.style.display="none";
             }
         });        
-    }
-    else{
+        }
+        else if(e.target.innerText=="1bhk"){
+        items.forEach(val => { 
+            let cost=val.getElementsByClassName("bhk")[0].innerText;
+            console.log(cost);
+            if(cost[0]>1)
+            {
+                val.style.display="none";
+            }
+        });   
+        }
+        else if(e.target.innerText=="2bhk"){
+        items.forEach(val => { 
+            let cost=val.getElementsByClassName("bhk")[0].innerText;
+            console.log(cost);
+            if(cost[0]>2 || cost[0]<2)
+            {
+                val.style.display="none";
+            }
+        });   
+        }
+        else if(e.target.innerText=="3bhk"){
+        items.forEach(val => { 
+            let cost=val.getElementsByClassName("bhk")[0].innerText;
+            console.log(cost);
+            if(cost[0]>3 || cost[0]<3)
+            {
+                val.style.display="none";
+            }
+        });   
+        }
+        else if(e.target.innerText=="above 3bhk"){
+        items.forEach(val => { 
+            let cost=val.getElementsByClassName("bhk")[0].innerText;
+            console.log(cost);
+            if(cost[0]<4)
+            {
+                val.style.display="none";
+            }
+        });   
+        }
+        else{
         items.forEach(val => { 
             let cost=val.getElementsByClassName("bottom")[0].getElementsByTagName("div")[0].innerText;
             console.log(cost.substring(1));
@@ -53,13 +101,16 @@ drop_down_bud.forEach(element => {
                 val.style.display="none";
             }
         });        
-    }
+        }
     
     }
     else{
         drp_btn.innerHTML="Budget";
+        drp_btn2.innerHTML="Size-bhk";
     }
     
     })
 });
 // items[0]].style.display=none;
+
+
